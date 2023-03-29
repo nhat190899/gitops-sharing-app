96af73ea19c66b7f9e04226627033df5d5674e40
Trần Hoài Nhật <nhat190899@Trans-MacBook-Pro.local>
2023-03-29 13:13:40 +0700

    fix cicdjob

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to FSoft-COE-HCM sharing meeting sprint 2"/>
+  <HelloWorld msg="Welcome to FSoft-COE-HCM sharing meeting"/>
 </template>
 
 <script>
